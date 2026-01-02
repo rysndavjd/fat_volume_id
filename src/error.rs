983c2272a038ce9fb37d66013bb4cb462d5d1a16
rysndavjd <rysndavjd@proton.me>
2026-01-02 18:32:55 +0200

    2

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,156 @@
+use crate::std::fmt;
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Error(pub(crate) ErrorKind);
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub(crate) enum ErrorKind {
+pub enum ErrorKind {
+    /// Invalid character in given string.
+    ParseChar { character: char, index: usize },
+    /// Invalid length of given string.
+    ParseSimpleLength { len: usize },
+    /// Invalid length of given byte array.
     ParseByteLength { len: usize },
-    ParseInvalidAscii,
+    /// Invalid number of groups in hyphenated variant.
+    ParseGroupCount { count: usize },
+    /// Invalid length of a group within a hyphenated variant.
+    ParseGroupLength {
+        group: usize,
+        len: usize,
+        index: usize,
+    },
+    /// The input was not a valid UTF8 string
+    ParseInvalidUTF8,
+    /// Some other parsing error occurred.
+    ParseOther,
+}
+
+/// A string that is guaranteed to fail to parse to a [`VolumeId32`] or [`VolumeId64`].
+///
+/// This type acts as a lightweight error indicator, suggesting
+/// that the string cannot be parsed but offering no error
+/// details. To get details, use `InvalidVolumeId::into_err`.
+///
+/// [`VolumeId32`]: ../struct.VolumeId32.html
+/// [`VolumeId64`]: ../struct.VolumeId64.html
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct InvalidVolumeId<'a>(pub(crate) &'a [u8]);
+
+impl<'a> InvalidVolumeId<'a> {
+    /// Converts the lightweight error type into detailed diagnostics.
+    pub fn into_err(self) -> Error {
+        // Check whether or not the input was ever actually a valid UTF8 string
+        let input_str = match std::str::from_utf8(self.0) {
+            Ok(s) => s,
+            Err(_) => return Error(ErrorKind::ParseInvalidUTF8),
+        };
+
+        let mut hyphen_count = 0;
+        let mut group_bounds = [0; 4];
+
+        for (index, character) in input_str.char_indices() {
+            let byte = character as u8;
+            if character as u32 - byte as u32 > 0 {
+                // Multibyte char
+                return Error(ErrorKind::ParseChar {
+                    character,
+                    index: index + 1,
+                });
+            } else if byte == b'-' {
+                // While we search, also count group breaks
+                if hyphen_count < 1 {
+                    group_bounds[hyphen_count] = index;
+                }
+                hyphen_count += 1;
+            } else if !byte.is_ascii_hexdigit() {
+                // Non-hex char
+                return Error(ErrorKind::ParseChar {
+                    character: byte as char,
+                    index: index + 1,
+                });
+            }
+        }
+
+        if hyphen_count == 0 {
+            // This means that we tried and failed to parse a simple uuid.
+            // Since we verified that all the characters are valid, this means
+            // that it MUST have an invalid length.
+            Error(ErrorKind::ParseSimpleLength {
+                len: input_str.len(),
+            })
+        } else if hyphen_count != 1 {
+            // We tried to parse a hyphenated variant, but there weren't
+            // 2 groups (1 hyphen splits).
+            Error(ErrorKind::ParseGroupCount {
+                count: hyphen_count + 1,
+            })
+        } else {
+            // There are 2 groups, one of them has an incorrect length
+            const BLOCK_STARTS: [usize; 2] = [0, 5];
+            for i in 0..2 {
+                if group_bounds[i] != BLOCK_STARTS[i + 1] - 1 {
+                    return Error(ErrorKind::ParseGroupLength {
+                        group: i,
+                        len: group_bounds[i] - BLOCK_STARTS[i],
+                        index: BLOCK_STARTS[i] + 1,
+                    });
+                }
+            }
+
+            unreachable!()
+            // // The last group must be too long
+            // Error(ErrorKind::ParseGroupLength {
+            //     group: 4,
+            //     len: input_str.len() - BLOCK_STARTS[4],
+            //     index: BLOCK_STARTS[4] + 1,
+            // })
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.0 {
+            ErrorKind::ParseChar {
+                character, index, ..
+            } => {
+                write!(
+                    f,
+                    "invalid character: expected [0-9a-fA-F-], found `{}` at {}",
+                    character, index
+                )
+            }
+            ErrorKind::ParseSimpleLength { len } => {
+                write!(
+                    f,
+                    "invalid length: expected length for simple format, found {}",
+                    len
+                )
+            }
+            ErrorKind::ParseByteLength { len } => {
+                write!(f, "invalid byte length, found {}", len)
+            }
+            ErrorKind::ParseGroupCount { count } => {
+                write!(f, "invalid group count: expected 2, found {}", count)
+            }
+            ErrorKind::ParseGroupLength { group, len, .. } => {
+                let expected = [8, 4, 4, 4, 12][group];
+                write!(
+                    f,
+                    "invalid group length in group {}: expected {}, found {}",
+                    group, expected, len
+                )
+            }
+            ErrorKind::ParseInvalidUTF8 => write!(f, "non-UTF8 input"),
+            ErrorKind::ParseOther => write!(f, "failed to parse a UUID"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+mod std_support {
+    use super::*;
+    use crate::std::error;
+
+    impl error::Error for Error {}
 }
--- src/fmt.rs
+++ src/fmt.rs
@@ -10,11 +10,13 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use core::str::FromStr;
-
 use crate::{
     Error, VolumeId32,
-    std::{fmt, str},
+    std::{
+        fmt,
+        hash::{Hash, Hasher},
+        mem::transmute,
+    },
 };
 
 #[cfg(feature = "std")]
@@ -57,10 +59,188 @@ impl fmt::UpperHex for VolumeId32 {
     }
 }
 
-impl FromStr for VolumeId32 {
-    type Err = Error;
+impl Hash for VolumeId32 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write(&self.0);
+    }
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        return Self::try_parse(s);
+/// Format a [`VolumeId32`] as a simple string, like
+/// `6ddcf6da`.
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(
+    feature = "zerocopy",
+    derive(
+        zerocopy::IntoBytes,
+        zerocopy::FromBytes,
+        zerocopy::KnownLayout,
+        zerocopy::Immutable,
+        zerocopy::Unaligned
+    )
+)]
+#[repr(transparent)]
+pub struct SimpleId32(VolumeId32);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(
+    feature = "zerocopy",
+    derive(
+        zerocopy::IntoBytes,
+        zerocopy::FromBytes,
+        zerocopy::KnownLayout,
+        zerocopy::Immutable,
+        zerocopy::Unaligned
+    )
+)]
+#[repr(transparent)]
+pub struct HyphenatedId32(VolumeId32);
+
+impl VolumeId32 {
+    #[inline]
+    pub const fn simple(self) -> SimpleId32 {
+        SimpleId32(self)
+    }
+
+    #[inline]
+    pub fn as_simple(&self) -> &SimpleId32 {
+        unsafe { transmute(self) }
+    }
+
+    #[inline]
+    pub const fn hyphenated(self) -> HyphenatedId32 {
+        HyphenatedId32(self)
+    }
+
+    #[inline]
+    pub fn as_hyphenated(&self) -> &HyphenatedId32 {
+        unsafe { transmute(self) }
+    }
+}
+
+const UPPER: [u8; 16] = [
+    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'A', b'B', b'C', b'D', b'E', b'F',
+];
+const LOWER: [u8; 16] = [
+    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'a', b'b', b'c', b'd', b'e', b'f',
+];
+
+#[inline]
+const fn format_simpleid32(src: &[u8; 4], upper: bool) -> [u8; SimpleId32::LENGTH] {
+    let lut = if upper { &UPPER } else { &LOWER };
+    let mut dst = [0; SimpleId32::LENGTH];
+    let mut i = 0;
+    while i < 16 {
+        let x = src[i];
+        dst[i * 2] = lut[(x >> 4) as usize];
+        dst[i * 2 + 1] = lut[(x & 0x0f) as usize];
+        i += 1;
+    }
+    dst
+}
+
+#[inline]
+const fn format_hyphenatedid32(src: &[u8; 4], upper: bool) -> [u8; HyphenatedId32::LENGTH] {
+    let lut = if upper { &UPPER } else { &LOWER };
+    let groups = [(0, 4), (5, 8)];
+    let mut dst = [0; HyphenatedId32::LENGTH];
+
+    let mut group_idx = 0;
+    let mut i = 0;
+    while group_idx < 2 {
+        let (start, end) = groups[group_idx];
+        let mut j = start;
+        while j < end {
+            let x = src[i];
+            i += 1;
+
+            dst[j] = lut[(x >> 4) as usize];
+            dst[j + 1] = lut[(x & 0x0f) as usize];
+            j += 2;
+        }
+        if group_idx < 1 {
+            dst[end] = b'-';
+        }
+        group_idx += 1;
+    }
+    dst
+}
+
+impl SimpleId32 {
+    pub const LENGTH: usize = 8;
+
+    pub const fn from_volumeid32(uuid: VolumeId32) -> Self {
+        SimpleId32(uuid)
+    }
+
+    #[inline]
+    pub fn encode_lower<'buf>(&self, buffer: &'buf mut [u8]) -> &'buf mut str {
+        Self::_encode_simpleid32(self.0.as_bytes(), buffer, false)
+    }
+
+    #[inline]
+    pub fn encode_upper<'buf>(&self, buffer: &'buf mut [u8]) -> &'buf mut str {
+        Self::_encode_simpleid32(self.0.as_bytes(), buffer, true)
+    }
+
+    #[inline]
+    fn _encode_simpleid32<'b>(src: &[u8; 4], buffer: &'b mut [u8], upper: bool) -> &'b mut str {
+        assert!(
+            buffer.len() >= Self::LENGTH,
+            "Buffer too small to encode a SimpleId32"
+        );
+
+        let buf: &mut [u8; Self::LENGTH] = buffer[..Self::LENGTH].as_mut_array().unwrap();
+        *buf = format_simpleid32(src, upper);
+
+        // SAFETY: The encoded buffer is ASCII encoded
+        unsafe { str::from_utf8_unchecked_mut(buf) }
+    }
+
+    pub const fn as_volumeid32(&self) -> &VolumeId32 {
+        &self.0
+    }
+
+    pub const fn into_volumeid32(self) -> VolumeId32 {
+        self.0
+    }
+}
+
+impl HyphenatedId32 {
+    pub const LENGTH: usize = 9;
+
+    pub const fn from_volumeid32(uuid: VolumeId32) -> Self {
+        HyphenatedId32(uuid)
+    }
+
+    #[inline]
+    pub fn encode_lower<'buf>(&self, buffer: &'buf mut [u8]) -> &'buf mut str {
+        Self::_encode_hyphenatedid32(self.0.as_bytes(), buffer, false)
+    }
+
+    #[inline]
+    pub fn encode_upper<'buf>(&self, buffer: &'buf mut [u8]) -> &'buf mut str {
+        Self::_encode_hyphenatedid32(self.0.as_bytes(), buffer, true)
+    }
+
+    #[inline]
+    fn _encode_hyphenatedid32<'b>(src: &[u8; 4], buffer: &'b mut [u8], upper: bool) -> &'b mut str {
+        assert!(
+            buffer.len() >= Self::LENGTH,
+            "Buffer too small to encode a SimpleId32"
+        );
+
+        let buf: &mut [u8; Self::LENGTH] = buffer[..Self::LENGTH].as_mut_array().unwrap();
+        *buf = format_hyphenatedid32(src, upper);
+
+        // SAFETY: The encoded buffer is ASCII encoded
+        unsafe { str::from_utf8_unchecked_mut(buf) }
+    }
+
+    pub const fn as_volumeid32(&self) -> &VolumeId32 {
+        &self.0
+    }
+
+    pub const fn into_volumeid32(self) -> VolumeId32 {
+        self.0
     }
 }
